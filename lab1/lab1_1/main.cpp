450c9b51aa8fcb52b75547caae50f0f2fdd8a1c6
Roman Astrakhantsev <rgastrakhantsev@gmail.com>
2021-11-05 03:24:50 +0300

    lab1.1: vector operation measurment

--- lab1/lab1_1/main.cpp
+++ lab1/lab1_1/main.cpp
@@ -6,6 +6,8 @@
 #include <chrono>
 #include <tuple>
 
+#include <immintrin.h>
+
 using namespace std;
 
 namespace hse {
@@ -41,14 +43,15 @@ using time_t = std::chrono::duration<double, std::milli>;
 
 }
 
-tuple<double, hse::time_t> measure_multipy_single(const vector<double>& data){
+tuple<double, hse::time_t> measure_multipy_single(const vector<double>& x,
+                                                  const vector<double>& y){
 
     double result = 1.;
 
     auto start = chrono::steady_clock::now();
 
-    for(size_t i=0; i<data.size(); ++i){
-        result *= data[i];
+    for(size_t i=0; i< x.size(); ++i){
+        result *= (x[i] * y[i] );
     }
 
     auto end = chrono::steady_clock::now();
@@ -57,7 +60,32 @@ tuple<double, hse::time_t> measure_multipy_single(const vector<double>& data){
 
 }
 
-void measure_multipy_vector(){
+tuple<double, hse::time_t> measure_multipy_vector(const vector<double>& x,
+                                                  const vector<double>& y){
+
+    const __m128d *x_vec = reinterpret_cast<const __m128d *>(x.data()),
+                  *y_vec = reinterpret_cast<const __m128d *>(y.data());
+
+    __m128d p = _mm_set1_pd(1.);
+
+    constexpr size_t v_len = (128/8/sizeof(double));
+
+    // start
+    auto start = chrono::steady_clock::now();
+
+    for(int i=0; i< x.size()/v_len; ++i){
+        __m128d tmp = _mm_mul_pd(x_vec[i], y_vec[i]);
+        p = _mm_mul_pd(p, tmp);
+    }
+
+    double result = 1.;
+    for (size_t i=0; i<v_len; ++i)
+        result *= (reinterpret_cast<double*>(&p))[i];
+
+    //end
+    auto end = chrono::steady_clock::now();
+
+    return {result, end-start};
 
 }
 
@@ -65,11 +93,14 @@ void measure_multipy_vector(){
 int main(int arc, char* argv[])
 {
     size_t n = hse::parse_n(argv[1]);
-    vector<double> data = hse::generate_random(n);
+    vector<double> x = hse::generate_random(n);
+    vector<double> y = hse::generate_random(n);
 
-    auto [result, time] = measure_multipy_single(data);
+    auto [result_s, time_s] = measure_multipy_single(x, y);
+    auto [result_v, time_v] = measure_multipy_vector(x, y);
 
     cout << "result" "\t" "n" "\t" "latency" "\t" "IPS" << endl;
-    cout << result << "\t" << data.size() << "\t" << time.count() << "\t" << (data.size() / time.count()) << endl;
+    cout << result_s << "\t" << n << "\t" << time_s.count() << "\t" << (n / time_s.count()) << endl;
+    cout << result_v << "\t" << n << "\t" << time_v.count() << "\t" << (n / time_v.count()) << endl;
     return 0;
 }
